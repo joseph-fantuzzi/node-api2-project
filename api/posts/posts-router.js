c9f452005b13a3e74367d26ec13a7a0b46fcec55
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-04-12 16:13:22 -0400

    put request in place

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -54,4 +54,30 @@ router.post("/", (req, res) => {
   }
 });
 
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedPost = req.body;
+  if (!updatedPost.title || !updatedPost.contents) {
+    res.status(400).json({ message: "Please provide title and contents for the post" });
+  } else {
+    DB.update(id, updatedPost)
+      .then((count) => {
+        if (count !== 1) {
+          res.status(404).json({ message: "The post with the specified ID does not exist" });
+        } else {
+          DB.findById(id)
+            .then((post) => {
+              res.status(200).json(post);
+            })
+            .catch((err) => {
+              res.status(500).json({ message: "The post information could not be retrieved" });
+            });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({ message: "The post information could not be modified" });
+      });
+  }
+});
+
 module.exports = router;
