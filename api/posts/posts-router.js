7aea7209f32a523e931afd13f08ad097d2b7542f
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-04-12 15:50:55 -0400

    post request in place

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -31,4 +31,27 @@ router.get("/:id", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  const post = req.body;
+  if (!post.title || !post.contents) {
+    res.status(400).json({ message: "Please provide title and contents for the post" });
+  } else {
+    DB.insert(post)
+      .then((newPost) => {
+        DB.findById(newPost.id)
+          .then((post) => {
+            res.status(201).json(post);
+          })
+          .catch((err) => {
+            res.status(500).json({ message: "The post information could not be retrieved" });
+          });
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .json({ message: "There was an error while saving the post to the database" });
+      });
+  }
+});
+
 module.exports = router;
