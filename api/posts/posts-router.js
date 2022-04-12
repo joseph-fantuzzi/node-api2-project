b85571bf4594b656fc76a56b6acaabea851495bc
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-04-12 16:54:02 -0400

    comments get request in place

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -103,4 +103,29 @@ router.delete("/:id", (req, res) => {
     });
 });
 
+router.get("/:id/comments", (req, res) => {
+  const { id } = req.params;
+  DB.findPostComments(id)
+    .then((comments) => {
+      if (comments.length === 0) {
+        DB.findById(id)
+          .then((post) => {
+            if (!post) {
+              res.status(404).json({ message: "The post with the specified ID does not exist" });
+            } else {
+              res.status(404).json({ message: "The post was found but has no comments" });
+            }
+          })
+          .catch((err) => {
+            res.status(500).json({ message: "The post information could not be retrieved" });
+          });
+      } else {
+        res.status(200).json(comments);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "The comments information could not be retrieved" });
+    });
+});
+
 module.exports = router;
