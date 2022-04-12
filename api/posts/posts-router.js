31f743d37a78f8c4627f888b205972873dc3a55d
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-04-12 16:29:14 -0400

    delete request in place

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -80,4 +80,27 @@ router.put("/:id", (req, res) => {
   }
 });
 
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  let deletedPost;
+  DB.findById(id)
+    .then((post) => {
+      deletedPost = post;
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "The post information could not be retrieved" });
+    });
+  DB.remove(id)
+    .then((number) => {
+      if (number !== 1) {
+        res.status(404).json({ message: "The post with the specified ID does not exist" });
+      } else {
+        res.status(200).json(deletedPost);
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "The post could not be removed" });
+    });
+});
+
 module.exports = router;
